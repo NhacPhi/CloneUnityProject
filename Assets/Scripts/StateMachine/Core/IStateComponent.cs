b1d8fb8c5f007e0dd5318ba2ba946a05e4c2f0a7
NhacPhi <52564362+NhacPhi@users.noreply.github.com>
2024-04-06 22:52:35 +0700

    + Add FSM (State machine SO)

---
+++ Assets/Scripts/StateMachine/Core/IStateComponent.cs
@@ -0,0 +1,16 @@
+namespace StateMachine
+{
+    public interface IStateComponent 
+    {
+        /// <summary>
+		/// Called when entering the state.
+		/// </summary>
+		void OnStateEnter();
+
+        /// <summary>
+        /// Called when leaving the state.
+        /// </summary>
+        void OnStateExit();
+    }
+
+}
